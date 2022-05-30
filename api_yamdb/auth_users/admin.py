603a7a876deb313d4e372970977a8e85d587cabf
Alexandr Kroshko <akroshko1995@gmail.com>
2022-05-30 17:42:21 +0300

    model User added

---
+++ api_yamdb/auth_users/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import MyUser
+
+
+@admin.register(MyUser)
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'role')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'bio')}),
+        (_('Permissions'), {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions'
+            ),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    list_display = ('username', 'email', 'role')
---
+++ api_yamdb/auth_users/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class MyUser(AbstractUser):
+    email = models.EmailField(_('email address'), blank=False, null=False, max_length=254)
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    bio = models.TextField(blank=True)
+
+    anon = 'anon'
+    user = 'user'
+    moderator = 'moderator'
+    admin = 'admin'
+
+    ROLE_CHOICES = [
+        (anon, 'Anonymous User'),
+        (user, 'Authenticated User'),
+        (moderator, 'Moderator'),
+        (admin, 'Administrator'),
+    ]
+
+    role = models.CharField(choices=ROLE_CHOICES, default=user, max_length=9)
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        if self.is_staff is True:
+            self.role = self.admin
+        super().save(*args, **kwargs)
