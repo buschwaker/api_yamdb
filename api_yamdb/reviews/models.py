3714c676ec41124db6102cf0a8bdfd7333ffc4c9
Кирсанов Роман <romat42ru@gmail.com>
2022-06-01 00:46:44 +0700

    Написал модели Review и Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,10 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class MyUser(AbstractUser):
-    email = models.EmailField(_('email address'), blank=False, null=False, max_length=254)
+    email = models.EmailField(
+        _('email address'), blank=False, null=False, max_length=254)
     first_name = models.CharField(_('first name'), max_length=150, blank=True)
     last_name = models.CharField(_('last name'), max_length=150, blank=True)
     bio = models.TextField(blank=True)
@@ -30,3 +32,48 @@ class MyUser(AbstractUser):
         if self.is_staff is True:
             self.role = self.admin
         super().save(*args, **kwargs)
+
+
+class Review(MyUser):
+    """Модель отзыва."""
+    text = models.TextField(_('text'))
+    score = models.IntegerField(
+        _('score'),
+        validators=[
+            MinValueValidator(1, 'Минимальная оценка: 1'),
+            MaxValueValidator(10, 'Максимальная оценка: 10')
+        ]
+    )
+    title = models.ForeignKey(_('title'), Title, on_delete=models.CASCADE)
+    author = models.ForeignKey(_('author'), MyUser, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(
+        _('publishing date'), auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.text[:10]
+
+    class Meta:
+        # Устаревший вариант, думаю не стоит юзать.
+        # unique_together = ('author', 'title')
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_review'
+            )
+        ]
+        ordering = ['-pub_date']
+
+
+class Comment(MyUser):
+    """Модель комментария."""
+    review = models.ForeignKey(
+        _('review'), Review, on_delete=models.CASCADE, related_name='comments')
+    title = models.ForeignKey(_('title'), Title, on_delete=models.CASCADE)
+    text = models.TextField(_('text'))
+    author = models.ForeignKey(_('author'), MyUser, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(
+        _('publishing date'), auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.text[:10]
